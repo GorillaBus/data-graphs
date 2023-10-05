05a418cabc450f964ac27da634e1c65fabe95774
gorillabus <eduugr@gmail.com>
2023-10-05 21:17:11 +0200

    [backend-api] Added CORS plugin for Flask

--- packages/backend-api/app/infrastructure/bootstrap/create_app.py
+++ packages/backend-api/app/infrastructure/bootstrap/create_app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import bp as routes_bp
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(routes_bp)
     return app
