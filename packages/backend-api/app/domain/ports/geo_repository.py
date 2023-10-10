304a16db90e5a39e2f0616e7f4ccad50d044fcf6
gorillabus <eduugr@gmail.com>
2023-10-11 01:39:20 +0200

    [backend-api] interface IGeoRepository: find_ways_in_radius returns IGisFeature

--- packages/backend-api/app/domain/ports/geo_repository.py
+++ packages/backend-api/app/domain/ports/geo_repository.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple
-from app.domain.definitions.gis import TGisFeature
+from app.infrastructure.dto.node_dto import NodeDTO  # Controlled coupling!
 
 
 class IGeoRepository(ABC):
 
     @abstractmethod
-    def find_ways_in_radius(self, center_coords: Tuple[float, float], radius: float) -> List[TGisFeature]:
+    def find_ways_in_radius(self, center_coords: Tuple[float, float], radius: float) -> List[NodeDTO]:
         pass
