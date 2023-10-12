4019f682433bee13f94d0138a4b5c8f1556efb0c
gorillabus <eduugr@gmail.com>
2023-10-12 19:55:35 +0200

    [backend-api] routes: fixed negative floats on routes

--- packages/backend-api/app/infrastructure/bootstrap/routes.py
+++ packages/backend-api/app/infrastructure/bootstrap/routes.py
@@ -4,6 +4,6 @@ from app.application.controllers.find_path_controller import FindPathController
 bp = Blueprint('routes', __name__)
 
 
-@bp.route('/api/find-path/<float:lat_a>/<float:lon_a>/<float:lat_b>/<float:lon_b>', methods=['GET'])
+@bp.route('/api/find-path/<float(signed=True):lat_a>/<float(signed=True):lon_a>/<float(signed=True):lat_b>/<float(signed=True):lon_b>', methods=['GET'])
 def find_path_route(lat_a, lon_a, lat_b, lon_b):
     return FindPathController.handle_request(lat_a, lon_a, lat_b, lon_b)
