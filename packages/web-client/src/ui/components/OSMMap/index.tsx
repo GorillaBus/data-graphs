2495928d0a7abae8e3b854f17c558394850d3e8a
gorillabus <eduugr@gmail.com>
2023-10-12 19:58:21 +0200

    [web-client] <bullk> OSMMap: defaultPosition, No deafult popup, errors

--- packages/web-client/src/ui/components/OSMMap/index.tsx
+++ packages/web-client/src/ui/components/OSMMap/index.tsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState, useEffect } from "react";
-import { MapContainer, Marker, Popup, TileLayer, GeoJSON, Polyline } from "react-leaflet";
 import L from 'leaflet';
+import { TNode } from "@web-client/domain/definitions/gis"
+import { MapContainer, Marker, TileLayer, Polyline } from "react-leaflet";
+
 import 'leaflet/dist/leaflet.css';
 import './style.css';
 
@@ -11,8 +13,8 @@ L.Icon.Default.mergeOptions({
 });
 
 const defaultPosition = {
-  lat: 35.7407872,
-  lng: 51.4375991,
+  lat: -34.7833,
+  lng: -58.4000,
   zoom: 13
 };
 
@@ -21,39 +23,45 @@ type MarkerData = {
   label: string,
 }
 const markersData: MarkerData[] = [
-  { position: [35.745, 51.430], label: 'A' },
-  { position: [35.735, 51.440], label: 'B' }
+  { position: [-34.7833, -58.4002], label: 'A' },
+  { position: [-34.7839, -58.4038], label: 'B' }
 ];
 
 export const OSMMap: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const markersRef = useRef<{[key: string]: any}>({});
-  const [areaFeatures, setAreaFeatures] = useState<any>(null);
+  const [areaFeatures, setAreaFeatures] = useState<TNode[]>(null);
   const [markersReady, setMarkersReady] = useState<boolean>(false);
 
+  
   const fetchFeatures = async () => {
+    setIsLoading(true);
+    setError(null);
+
     const markerA = markersRef.current['A'].getLatLng();
     const markerB = markersRef.current['B'].getLatLng();
-
     try {
-      const response = await fetch(`http://127.0.0.1:5000/api/find-path/${markerA.lat}/${markerA.lng}/${markerB.lat}/${markerB.lng}`);
-      
-      if (!response.ok) {
-        const errorData = await response.json();
-        alert(`Error: ${errorData.error}`);
-        return;
-      }
-      
-      const data = await response.json();
-      
-      setAreaFeatures(data.path);      
+        const response = await fetch(`http://127.0.0.1:5000/api/find-path/${markerA.lat}/${markerA.lng}/${markerB.lat}/${markerB.lng}`);        
+        if (!response.ok) {
+            const errorData = await response.json();
+            setError(errorData.error);
+            return;
+        }
+        
+        const data = await response.json();
+        setAreaFeatures(data.path);      
 
     } catch (error) {
+        setError("An error occurred while requesting data");
+    } finally {
+      setIsLoading(false);
+  }
 
-      alert("An error occurred while requesting data");
-    }
+    setIsLoading(false);
   };
 
-  const handleDragEnd = async (label: string) => {
+  const handleDragEnd = async () => {
     await fetchFeatures();
   };
 
@@ -71,39 +79,75 @@ export const OSMMap: React.FC = () => {
   }
 
   return (
-
     <div style={{ height: "100vh", width: "100%" }}>
-      
       <MapContainer
         className="map"
         center={defaultPosition}
         zoom={defaultPosition.zoom}
         style={{ height: "100%", width: "100%" }}
+        dragging={!isLoading}
+        zoomControl={!isLoading}
       >
 
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+
         {markersData.map(marker => (
-          <Marker
-            key={marker.label}
-            ref={ref => handleMarkerRef(marker.label, ref)}
-            position={marker.position}
-            draggable={true}
-            eventHandlers={{ dragend: () => handleDragEnd(marker.label) }}>
-            <Popup>This is position {marker.label}</Popup>
-          </Marker>
+            <Marker
+                key={marker.label}
+                ref={ref => handleMarkerRef(marker.label, ref)}
+                position={marker.position}
+                draggable={!isLoading}
+                eventHandlers={{ dragend: () => handleDragEnd() }}
+            >
+            </Marker>
         ))}
+
+        {/* Path polyline */}
         {
-            areaFeatures &&
-                <Polyline
-                    key='path'
-                    positions={areaFeatures.map((node: any) => [node.lat, node.lon])}
-                    color="blue"
-                />
+          areaFeatures &&
+              <Polyline
+                  key='path'
+                  positions={areaFeatures.map((node: TNode) => [node.lat, node.lon])}
+                  color="blue"
+              />
         }
       </MapContainer>
+
+      {/* Loader */}
+      {isLoading && (
+            <div style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: 1000,
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                padding: '20px',
+                borderRadius: '8px',
+            }}>
+                Loading...
+            </div>
+        )}
+
+      {/* Errors */}
+      {error && (
+          <div style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 1000,
+              backgroundColor: 'rgba(255, 255, 255, 0.8)',
+              padding: '20px',
+              borderRadius: '8px',
+          }}
+          onClick={() => setError(null)}>
+              {error}
+          </div>
+      )}
     </div>
   );
 }
