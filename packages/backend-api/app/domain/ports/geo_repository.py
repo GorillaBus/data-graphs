d892c61920635e022dbe83b22b703e13233da08e
gorillabus <eduugr@gmail.com>
2023-10-12 02:26:17 +0200

    [backend-api] GisGeoRepository: returning TGisFeature

--- packages/backend-api/app/domain/ports/geo_repository.py
+++ packages/backend-api/app/domain/ports/geo_repository.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple
-from app.domain.definitions.gis import TGisNode
+from app.domain.definitions.gis import TGisFeature
 
 
 class IGeoRepository(ABC):
 
     @abstractmethod
-    def find_ways_in_radius(self, center_coords: Tuple[float, float], radius: float) -> List[TGisNode]:
+    def find_ways_in_radius(self, center_coords: Tuple[float, float], radius: float) -> List[TGisFeature]:
         pass
